c61db35b24eccdbd8d020dd817e9d48c51520434
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-21 12:52:11 +0300

    es vs commonjs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-//const fs = require('fs'); // commonjs module
-import fs from 'fs'; // es module
-fs.writeFileSync('test.txt', 'Hello node!');
+const common = require('./common.js');
+common.hello();
+import es from './es.js';
+es.hello();
